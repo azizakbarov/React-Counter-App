0fcf99238d58ae39ff74d0489f6193da6e0fa1f7
Aziz Akbarov <azizbakbarov@gmail.com>
2021-10-27 20:51:01 +0500

    decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,11 +13,18 @@ class Counter extends Component {
             onClick={() => this.props.onIncrement(this.props.counter)}
             className="btn btn-secondary btn-sm"
           >
-            Increment
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
           </button>
           <button
             onClick={() => this.props.onDelete(this.props.counter.id)}
-            className="btn btn-danger btn-sm m-2"
+            className="btn btn-danger btn-sm"
           >
             Delete
           </button>
@@ -28,7 +35,7 @@ class Counter extends Component {
     );
   }
   getBadgeClasses() {
-    let classes = "badge m-2 badge-";
+    let classes = "badge badge-";
     classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends React.Component {
   render() {
-    const { onReset, counters, onDelete, onIncrement } = this.props;
+    const { onReset, counters, onDelete, onIncrement, onDecrement } = this.props;
     return (
       <div>
         <button className="btn btn-primary btn-sm m-2" onClick={onReset}>
@@ -14,6 +14,7 @@ class Counters extends React.Component {
             onDelete={onDelete}
             onIncrement={onIncrement}
             counter={counter}
+            onDecrement={onDecrement}
           />
         ))}
       </div>
